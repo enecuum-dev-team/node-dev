d5045c42c88e8a289cd495c28fb2c1a16b8de8d6
prudanoff <prudanoff@mail.ru>
2023-02-15 13:19:34 +0300

    sql tables

---
+++ sql/create_events_tables.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS `dex_history`;
+CREATE TABLE `dex_history` (
+  `hash` varchar(64) NOT NULL,
+  `action` varchar(45) NOT NULL,
+  `pool_id` varchar(130) DEFAULT NULL,
+  `block_n` varchar(45) NOT NULL,
+  `block_time` bigint(11) DEFAULT NULL,
+  `caller` varchar(66) DEFAULT NULL,
+  `v1_at` varchar(45) DEFAULT NULL,
+  `v2_at` varchar(45) DEFAULT NULL,
+  `tvl1` varchar(45) DEFAULT NULL,
+  `tvl2` varchar(45) DEFAULT NULL,
+  `lt_change` varchar(45) DEFAULT NULL,
+  `i` bigint(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`i`),
+  UNIQUE KEY `i_UNIQUE` (`i`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE `events` (
+  `type` varchar(45) DEFAULT NULL,
+  `hash` varchar(64) DEFAULT NULL,
+  `time` bigint(11) DEFAULT NULL,
+  `n` bigint(11) DEFAULT NULL,
+  `data` varchar(500) DEFAULT NULL,
+  `i` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`i`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
